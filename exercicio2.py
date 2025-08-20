54a23a7dd0678deb0373da52d44a9bbf9cb2feea
thiago de mattos <thiagodemattosazevedochaves1@gmail.com>
2025-08-20 11:33:46 -0300

    commit bomba

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+preco = float(input('digite o preço original do preoduto: '))
+
+if preco > 100:
+    porcentagem = preco * 0.9
+print(porcentagem)
+
---
+++ exercicio4.py
@@ -0,0 +1,8 @@
+senha_secreta = 'python'
+
+senha = input('digite a sua senha: ')
+
+if senha == senha_secreta:
+    print('senha correta')
+else:
+    print('senha incorreta')
