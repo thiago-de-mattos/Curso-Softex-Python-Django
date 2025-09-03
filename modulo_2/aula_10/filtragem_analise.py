4dcf9b32119ef0210c08520a16f39b0f4f2e3fe6
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-09-03 16:36:36 -0300

    Exercicio filtragem de produtos usando tuplas

--- modulo_2/aula_10/filtragem_analise.py
+++ modulo_2/aula_10/filtragem_analise.py
@@ -2,21 +2,29 @@ class Analise:
 
     def __init__(self):
         self.produtos_unicos = set()
-        self.vendas = [('Teclado', 50, 1),
-                                 ('Mouse', 25.50, 4),
-                                 ('Monitor', 700, 1),
-                                 ('Fone', 45, 1),
-                                 ('Webcan', 75.20, 2)]       
+        self.vendas = [
+            ('Teclado', 50, 1),
+            ('Mouse', 25.50, 4),
+            ('Monitor', 700, 1),
+            ('Fone', 45, 1),
+            ('Webcan', 75.20, 2)
+]       
         self.vendas_filtradas = []
 
     def filtragem(self):
-         for produto, preco, qtd in self.vendas:
-             self.soma = preco*qtd
-             if self.soma >=100:
-                self.vendas_filtradas.append(produto, preco, qtd)
+        for produto, preco, qtd in self.vendas:
+             total = preco*qtd
+             if total >=100:
+                self.vendas_filtradas.append((produto, preco, qtd, total))
                 self.produtos_unicos.add(produto)
-                
-        
 
+        return self.vendas_filtradas
+                
 analise = Analise()
-print(f'Vendas filtradas: {analise.filtragem()} ')
+resultado = analise.filtragem()
+
+print(f'Vendas filtradas: ')
+for produto, preco, qtd, total in resultado:
+    print(f" - {produto}: {qtd} un. x {preco:.2f} = {total:.2f}")
+
+print("\nProdutos únicos filtrados:", analise.produtos_unicos)
