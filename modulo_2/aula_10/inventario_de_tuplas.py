d92f1a91dd6782121f10886ed875ab748c0a020d
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-09-03 16:23:32 -0300

    Exercicio inventario de tuplas

--- modulo_2/aula_10/inventario_de_tuplas.py
+++ modulo_2/aula_10/inventario_de_tuplas.py
@@ -1,21 +1,36 @@
 class Loja:
 
     def __init__(self):
-        self.estoque_principal = [('Camisa', 101), ('Calça', 102),('bone', 103), ('Tenis',104)]
-        self.estoque_online = [('Bone', 103), ('Camisa Polo', 105), ('calça', 102), ('Chinelo',)]
-        self.novo_estoque_1 = set ()
-        self.novo_estoque_2 = set ()
-
+        self.estoque_principal = [
+            ('Camisa', 101),
+            ('Calça', 102),
+            ('Boné', 103),
+            ('Tênis', 104)
+        ]
+        self.estoque_online = [
+            ('Boné', 103),
+            ('Camisa Polo', 105),
+            ('Calça', 102),
+            ('Chinelo', 106)
+        ]
+        self.novo_estoque_1 = set()
+        self.novo_estoque_2 = set()
 
     def descubra(self):
-        em_comum = self.novo_estoque_1.intersection(self.novo_estoque_2)
-        print(f'Os produtos em comum{em_comum}')
-        principal = self.novo_estoque_1.difference(self.novo_estoque_2)
-        print(principal)
-        online = self.novo_estoque_2.difference(self.novo_estoque_1)
-        principal(online)
+        for nome, codigo in self.estoque_principal:
+            self.novo_estoque_1.add(codigo)
 
+        for nome, codigo in self.estoque_online:
+            self.novo_estoque_2.add(codigo)
+
+        em_comum = self.novo_estoque_1.intersection(self.novo_estoque_2)
+        print(f'Produtos em comum: {nome} {em_comum}')
+
+        apenas_principal = self.novo_estoque_1.difference(self.novo_estoque_2)
+        print(f'Apenas no estoque principal: {nome} {apenas_principal}')
+
+        apenas_online = self.novo_estoque_2.difference(self.novo_estoque_1)
+        print(f'Apenas no estoque online: {nome} {apenas_online}')
 
 loja = Loja()
-
 loja.descubra()
