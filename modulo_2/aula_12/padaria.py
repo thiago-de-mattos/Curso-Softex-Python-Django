d01706e670fc7f041427ccf4b2cf40647cfd3e76
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-09-12 17:11:11 -0300

    desafio da padaria melhorado

--- modulo_2/aula_12/padaria.py
+++ modulo_2/aula_12/padaria.py
@@ -1,103 +1,134 @@
-class Padaria:
-    def __init__(self):
-        self.quantidade_paes = {
-            'frances': 15,
-            'doce': 20,
-            'forma': 18
-        }
-        self.valor_paes = {
-            'frances': 0.50,
-            'doce': 0.70,
-            'forma': 1.20
-        }
-        self.bairros = {
-            '1': ('barroco', 5.00),
-            '2': ('sao_jose', 15.00)
-        }
-        self.codigo_venda = 98568
+"""
+Comercio Padaria
+x 1-O programa tem que rodar em loop infinito até ser parado
+x 2-cliente pedir um tipo de pão (frances, doce, forma, australiano)
+x 3-cada pao terá uma quantidade
+x 4-valor do pao
+x 5-pedir forma de pagameno (dinheiro, cartao)
+x 6-forma de entrega
+x 7-dados do cliente (se for entregar)
+x 8-valor do frete por bairro
+x 9-nome do atendente
+x 10-codigo da entrega
+"""
+def dados() -> dict:
+    """Carregar e retornar os dado de produtos, frete e funcionários"""
+    return {
+        "atendente": "Maria",
+        "paes": {
+            "frances": {"nome": "Pão Francês", "valor": 0.50, "quantidade": 15},
+            "doce": {"nome": "Pão Doce", "valor": 5.00, "quantidade": 20},
+            "forma": {"nome": "Pão de Forma", "valor": 5.99, "quantidade": 18},
+        },
+        "bairros": {
+            "barroco": {"nome": "Barroco", "frete": 5.00},
+            "sao jose": {"nome": "São José", "frete": 15.00},
+        },
+        "codigo_venda_base": 95875,
+    }
 
-    def dados(self) -> dict:
-        """Carregar e retornar um dicionário de dados do produto"""
-        return {
-            'atendente': "maria",
-            "paes": {
-                "frances": {"nome": "pao frances", "valor": 0.50},
-                "doce": {"nome": "pao doce", "valor": 0.70},
-                "forma": {"nome": "pao de forma", "valor": 1.20}
-            }
-        }
+def obter_dados_cliente() -> dict:
+    """Solicitar e retornar os dados do cliente"""
+    nome = input("Informe seu nome: ")
+    return {"nome": nome}
 
-    def escolher_pao(self):
-        nomes_pao = ', '.join(self.quantidade_paes.keys())
-        escolha = input(f'Temos os pães: {nomes_pao}. Qual pão você deseja?: ')
-        if escolha in self.quantidade_paes:
-            try:
-                quantidade = int(input(f'Quantos pães {escolha} você deseja? '))
-            except ValueError:
-                print('Quantidade inválida.')
-                return None, None
-            if quantidade <= self.quantidade_paes[escolha]:
-                self.quantidade_paes[escolha] -= quantidade
-                return escolha, quantidade
-            else:
-                print(f'Infelizmente só temos {self.quantidade_paes[escolha]} pães {escolha} disponíveis')
-                return None, None
+def solicitar_forma_pagamento() -> str:
+    """Solicitar e retornar a forma de pagamento escolhida"""
+    while True:
+        pagamento = input("Escolha a forma de pagamento (1-Dinheiro, 2-Cartão)")
+        if pagamento == "1":
+            return "Dinheiro"
+        elif pagamento == "2":
+            return "Cartão"
         else:
-            print('Pão não disponível.')
-            return None, None
+            print("Forma de pagamento inválida")
 
-    def calcular_valor(self, escolha, quantidade):
-        if escolha and quantidade:
-            return quantidade * self.valor_paes[escolha]
-        return 0
+def gerar_codigo_venda(codigo_base: int) -> int:
+    """Gera e retorna o código de venda"""
+    return codigo_base + 1
 
-    def escolher_entrega(self):
-        forma_retirada = input('É para entregar ou retirar? ')
-        if forma_retirada.lower() == 'entregar':
-            bairro_entrega = input('Qual o seu bairro? (1:barroco, 2:sao_jose) ')
-            if bairro_entrega in self.bairros:
-                bairro_nome, valor_frete = self.bairros[bairro_entrega]
-                print(f'Valor do frete para {bairro_nome} é {valor_frete}')
-                return valor_frete
-            else:
-                print('Fora da área de entrega')
-                return None
-        elif forma_retirada.lower() == 'retirar':
-            print('Você escolheu retirar o pedido')
-            return 0
-        else:
-            print('Opção inválida.')
-            return None
-
-    def solicitar_dados_cliente(self):
-        nome = input('Por favor, informe seu nome: ')
-        forma_pagamento = input('Qual a forma de pagamento? (1:dinheiro, 2:cartão) ')
-        forma_pagamento = 'dinheiro' if forma_pagamento == '1' else 'cartão'
-        return nome, forma_pagamento
-
-    def realizar_pedido(self):
-        dados = self.dados()
-        print(f'-- Bem-vindo à padaria Desespero, sou a atendente {dados["atendente"]} --')
-        escolha, quantidade = self.escolher_pao()
-        if not escolha or not quantidade:
-            return
-
-        valor_total_paes = self.calcular_valor(escolha, quantidade)
-        print(f'Você pediu {quantidade} pães {escolha}, o valor total é {valor_total_paes}')
-
-        valor_frete = self.escolher_entrega()
-        if valor_frete is None:
-            return
-
-        nome_cliente, forma_pagamento = self.solicitar_dados_cliente()
-        self.codigo_venda += 1
-
-        print(f'O valor total do seu pedido é: {valor_total_paes + valor_frete}')
-        print(f'Cliente: {nome_cliente}, Pagamento: {forma_pagamento}, Código do pedido: {self.codigo_venda}')
+def calcula_frete(bairros_disponives: dict) -> tuple[str, float]:
+    """Calcula o valor do frete com base no bairro de entrega"""
+    print("Bairros para entrega")
+    while True:
+        for bairro in bairros_disponives.values():
+            print(f"- {bairro["nome"]}" )
         
-        if __name__ == "__main__":
-            padaria = Padaria()
-            padaria.realizar_pedido()
+        bairro_entrega_nome = input("Qual o bairro de entrega?").lower()
 
-        print('bairo fora da area de entrega ')
+        bairro_encontrado = ""
+
+        for chave, bairro in bairros_disponives.values():
+            if bairro["nome"].lower() == bairro_entrega_nome:
+                bairro_encontrado = chave
+                break
+        
+        if not bairro_encontrado:
+            print("Bairro fora da área de entrega")
+        else:
+            frete = bairros_disponives[bairro_encontrado]["frete"]
+            return bairro_entrega_nome, frete
+        
+def cadastrar_produto(estoque: dict) -> None:
+    """Permite ao atendente cadastrar um novo produto"""
+    nome_produto = input("Digite o nome do novo produto (identificador)").lower().strip()
+
+    if nome_produto in estoque:
+        print("Erro! Produto já cadastrado com este identificador!!!")
         return
+
+    try:
+        nome_completo = input("Digite o nome completo do produto: ").strip()
+        valor = float(input("Digite o valor do novo produto: "))
+        quantidade = int(input("Digite a quantidade inicial do produto: "))
+
+        if nome_produto and nome_completo and valor > 0 and quantidade > 0:
+            estoque[nome_produto] = {"nome": nome_completo, "valor": valor, "quantidade": quantidade}
+            print(f"Produto {nome_completo} cadastrado com sucesso!")
+        
+        else:
+            print("Erro! Dados inválidos.")
+    
+    except ValueError:
+        print("Entrada de dados inválida.")
+
+def atualizar_produto(estoque: dict) -> None:
+    """Permite ao atendete atualizar um produto existente"""
+    nome_produto = input("Digite o nome do produto para atualizar (identificador): ").lower()
+
+    if nome_produto not in estoque:
+        print("Produto não cadastrado")
+        return
+    
+    print(f"Produto '{estoque[nome_produto]}' selecionado.")
+    escolha = input("O que deseja atualizar?\n \
+          1 - Valor\n \
+          2 - Quantidade")
+    
+    try:
+        if escolha == "1":
+            novo_valor = float(input("Digite o novo valor do produto: "))
+            if novo_valor > 0:
+                estoque[nome_produto]["valor"] = novo_valor
+                print(f"Valor atualizado para R$ {novo_valor:.2f} ")
+            else:
+                print("Valor inválido.")
+        elif escolha == "2":
+            nova_quantidade = int(input("Digite a nova quantidade do produto "))
+            if nova_quantidade > 0:
+                 estoque[nome_produto]["quantidade"] += nova_quantidade
+                 print(f"Quantidade atual de {estoque[nome_produto]["quantidade"]} itens.")
+            else:
+                print("Quantidade inválida! ")
+        else:
+            print("Erro! Opção inválida.")
+    except ValueError:
+        print("Erro! Entrada de dados inválida. Digite apenas números.")
+
+dados()
+obter_dados_cliente()
+solicitar_forma_pagamento()
+gerar_codigo_venda()
+calcula_frete()
+cadastrar_produto()
+atualizar_produto()
