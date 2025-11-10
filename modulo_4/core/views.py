33e5e21b099464d4c37a6d36739256bb330dbb80
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-11-10 11:37:48 -0300

    update: adicionando tarfe de execução

--- modulo_4/core/views.py
+++ modulo_4/core/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from .models import Tarefa
-# Create your views here.
-def home(request):
+from .models import Tarefa, Execucao
 
+def home(request):
+    todas_as_execucao = Execucao.objects.all()
     todas_as_tarefas = Tarefa.objects.all()
     context = {
         'nome_usuario': 'Thiago',
         'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
-        'tarefas': todas_as_tarefas
+        'tarefas': todas_as_tarefas,
+        'execucao': todas_as_execucao
+
     }
     return render(request, 'home.html', context)
 
