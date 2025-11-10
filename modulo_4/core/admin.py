3e92415cf8a49c8bb22db88e05097dfdc8bf1f02
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-11-10 11:37:16 -0300

    update: adicionando execucao

--- modulo_4/core/admin.py
+++ modulo_4/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tarefa
+from .models import Tarefa,Execucao
 
 # Register your models here.
 admin.site.register(Tarefa)
+admin.site.register(Execucao)
