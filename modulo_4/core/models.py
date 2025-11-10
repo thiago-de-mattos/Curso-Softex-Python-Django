287a789d89356d3f6f0d4df74608ca0f0b3cda95
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-11-10 11:37:48 -0300

    update: adicionando tarfe de execução

--- modulo_4/core/models.py
+++ modulo_4/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=200)
+    concluida = models.BooleanField(default=False)
+    criada_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+    
+class Execucao(models.Model):
+    nome = models.CharField(max_length=200)
+    local = models.CharField(max_length=200)
+    criada_em = models.DateTimeField(auto_now_add=True)
+    concluida = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'{self.nome} {self.local}'
