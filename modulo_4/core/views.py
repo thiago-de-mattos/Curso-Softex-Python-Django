8a00565078adf270778d8d7632d5b1044d7c1f2f
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-11-11 11:15:34 -0300

    update: adicionando tabela execução

--- modulo_4/core/views.py
+++ modulo_4/core/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from .models import Tarefa, Execucao
 
 def home(request):
-    todas_as_execucao = Execucao.objects.all()
+    todas_as_execucoes = Execucao.objects.all()
     todas_as_tarefas = Tarefa.objects.all()
     context = {
         'nome_usuario': 'Thiago',
         'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
         'tarefas': todas_as_tarefas,
-        'execucao': todas_as_execucao
-
+        'execucoes': todas_as_execucoes,
     }
     return render(request, 'home.html', context)
 
+
 def login(request):
-    return 
+    return render(request, 'login.html')
