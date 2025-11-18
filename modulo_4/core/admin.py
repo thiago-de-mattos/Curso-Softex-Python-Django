383097851d33cf908cf471b1fd3ac0d8c84421fc
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-11-18 18:38:45 -0300

    update: apagando tabela antigas

--- modulo_4/core/admin.py
+++ modulo_4/core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Tarefa,Execucao
+from .models import Tarefa
 
 # Register your models here.
 admin.site.register(Tarefa)
-admin.site.register(Execucao)
