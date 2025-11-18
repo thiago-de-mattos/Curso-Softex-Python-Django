b3949fc68cf190e1db481b0fa59313c872f3e2f1
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-11-18 18:39:14 -0300

    update: adicionando novas tabelas e funções

--- modulo_4/core/models.py
+++ modulo_4/core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 class Tarefa(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
     concluida = models.BooleanField(default=False)
     criada_em = models.DateTimeField(auto_now_add=True)
@@ -8,11 +10,3 @@ class Tarefa(models.Model):
     def __str__(self):
         return self.titulo
     
-class Execucao(models.Model):
-    nome = models.CharField(max_length=200)
-    local = models.CharField(max_length=200)
-    criada_em = models.DateTimeField(auto_now_add=True)
-    concluida = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return f'{self.nome} {self.local}'
--- modulo_4/core/views.py
+++ modulo_4/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Tarefa, Execucao
+from django.contrib.auth.forms import UserCreationForm # 1. Importe o form
+from django.contrib.auth import login # 2. Importe a função 'login'
+from django.contrib.auth.decorators import login_required # 1. Importe o decorado
+from .models import Tarefa
 from .forms import TarefaForm
+
+@login_required
 
 def home(request):
     if request.method == 'POST':
@@ -8,10 +13,14 @@ def home(request):
         form = TarefaForm(request.POST)
         # 4. O Django valida os dados (max_length, etc.)
         if form.is_valid():
-            # 5. Salva o objeto no banco de dados!
-            form.save()
+            # 'commit=False' cría o objeto na memória, mas não salva no banco.
+            tarefa = form.save(commit=False)
             # 6. Redireciona de volta para a 'home' 
             # Isso é o Padrão "Post-Redirect-Get" (PRG)
+            # Atribui o usuário logado (request.user) ao campo 'user' da tarefa
+            tarefa.user = request.user
+            # Agora sim, salva o objeto completo no banco
+            tarefa.save()
             return redirect('home')
         # Se o form NÃO for válido, o código continua e
         # o 'form' (com os erros) será enviado para o template
@@ -20,34 +29,31 @@ def home(request):
         form = TarefaForm()  # Cria um formulário vazio
 
     # 8. A busca de dados (fora dos 'ifs', pois é necessária sempre)
-    todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em')  # Ordena pelas mais novas
-    todas_as_execucoes = Execucao.objects.all()
+    todas_as_tarefas = Tarefa.objects.filter(user=request.user).order_by('-criada_em')  # Ordena pelas mais novas
 
     # 9. Atualize o contexto para incluir o formulário
     context = {
         'nome_usuario': 'Thiago',
         'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
         'tarefas': todas_as_tarefas,
-        'execucoes': todas_as_execucoes,
         'form': form,
     }
 
     return render(request, 'home.html', context)
 
+@login_required
 def concluir_tarefa(request, pk):
-    
-    tarefa = get_object_or_404(Tarefa, pk=pk)
-    # 2. Segurança: Apenas execute se o método for POST
+    # 2. Modifique o 'get_object_or_404'
+    # Busque a Tarefa pela 'pk' E ONDE o 'user' é o 'request.user'
+    tarefa = get_object_or_404(Tarefa, pk=pk, user=request.user)
     if request.method == 'POST':
-        # 3. A Lógica de "Update"
         tarefa.concluida = True
         tarefa.save() # Não se esqueça de salvar!
-        # 4. Redireciona de volta para a 'home' (Padrão PRG)
-        return redirect('home')
+    return redirect('home')
 
 def deletar_tarefa(request, pk):
     # 1. Busca a tarefa
-    tarefa = get_object_or_404(Tarefa, pk=pk)
+    tarefa = get_object_or_404(Tarefa, pk=pk, user=request.user)
     # 2. Segurança: Apenas execute se o método for POST
     if request.method == 'POST':
         # 3. A Lógica de "Delete"
@@ -55,5 +61,22 @@ def deletar_tarefa(request, pk):
         # 4. Redireciona de volta para a 'home'
         return redirect('home')
 
-def login(request):
-    return render(request, 'login.html')
+def register(request):
+    # Se a requisição for POST, o usuário enviou o formulário
+    if request.method == 'POST':
+        # Cria uma instância do formulário com os dados enviados
+        form = UserCreationForm(request.POST)
+    # Verifica se o formulário é válido (ex: senhas batem, username não existe)
+    if form.is_valid():
+        user = form.save() # Salva o novo usuário no banco
+        login(request, user) # Faz o login automático do usuário
+        return redirect('home')# Redireciona para a home
+    # Se a requisição for GET, o usuário apenas visitou a página
+    else:
+        form = UserCreationForm() # Cria um formulário de cadastro vazio
+
+    # Prepara o contexto e renderiza o template
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+
