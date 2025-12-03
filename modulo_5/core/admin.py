f964efd4e4dabb569508cace2ca5e000cbabe329
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-12-03 11:35:33 -0300

    feat: criando tarefa para admin

--- modulo_5/core/admin.py
+++ modulo_5/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Tarefa
 # Register your models here.
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'titulo', 'user', 'concluida', 'criada_em']
+    list_filter = ['concluida', 'criada_em']
+    search_fields = ['titulo', 'user__username']
