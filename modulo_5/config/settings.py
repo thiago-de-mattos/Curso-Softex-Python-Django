51a74d87115b320f65d4d1e828fed4ca94ebb3df
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-12-03 11:34:14 -0300

    feat: adicionando env

--- modulo_5/config/settings.py
+++ modulo_5/config/settings.py
@@ -16,16 +16,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-env = environ.Env(DEBUG=(bool, False) # Valor padrão caso não esteja no .env
-)
-
+env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
@@ -44,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'core',
-    
 ]
 
 MIDDLEWARE = [
