34811b24d934031968d278bbc3f9f960abe79c45
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-12-03 11:40:12 -0300

    feat: criando logica de listar tarefas e contar tarefas

--- modulo_5/core/views.py
+++ modulo_5/core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Tarefa
+from .serializers import TarefaSerializer
 
-# Create your views here.
+class ListaTarefasAPIView(APIView):
+
+    def get(self, request, format=None):
+        tarefas = Tarefa.objects.all()
+        serializer = TarefaSerializer(tarefas, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ContagemTarefasAPIView(APIView):
+   def get(self, request):
+        user_id = request.query_params.get('user_id')
+        if user_id:
+            tarefas = Tarefa.objects.filter(user_id=user_id)
+        else:
+            tarefas = Tarefa.objects.all()
+            serializer = TarefaSerializer(tarefas, many=True)
+        return Response(serializer.data)
+
