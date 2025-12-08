42901226d920b255051698ef8033abf85caec0a7
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-12-08 11:43:39 -0300

    voltando para antes

--- modulo_5/core/models.py
+++ modulo_5/core/models.py
@@ -2,20 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Tarefa(models.Model):
-    # MODIFICAÇÃO TEMPORÁRIA: null=True, blank=True
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        null=True, # Permite NULL no banco
-        blank=True, # Permite vazio em formulários
-        related_name='tarefas',
-        verbose_name='Usuário'
-        )
 
-    titulo = models.CharField(max_length=200, verbose_name='Título')
-    concluida = models.BooleanField(default=False, verbose_name='Concluída')
-    criada_em = models.DateTimeField(auto_now_add=True, verbose_name='Criada' \
-    'em')
+    user = models.ForeignKey(
+    User,
+    on_delete=models.CASCADE,
+    related_name='tarefas',
+    verbose_name='Usuário'
+    )
+
+    titulo = models.CharField(
+    max_length=200,
+    verbose_name='Título'
+    )
+
+    concluida = models.BooleanField(
+    default=False,
+    verbose_name='Concluída'
+    )
+
+    criada_em = models.DateTimeField(
+    auto_now_add=True,
+    verbose_name='Criada em'
+    )
 
     class Meta:
         verbose_name = 'Tarefa'
