7c4f6da90e46b3f89f161145204e04014c8bf811
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-12-09 16:57:48 -0300

    feat: path de EstatisticasTarefasAPIView

--- modulo_5/core/urls.py
+++ modulo_5/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListaTarefasAPIView
+from .views import ListaTarefasAPIView, EstatisticasTarefasAPIView
 
 app_name = 'core'
 urlpatterns = [
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
+    path('tarefas/estatisticas/', EstatisticasTarefasAPIView.as_view(), name='estatisticas'),
 ]
