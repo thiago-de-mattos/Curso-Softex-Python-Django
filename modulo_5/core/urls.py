2979e03773e953090f6307fcb9af485486d998c2
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-12-10 10:49:51 -0300

    feat: adicionando nova class da view DetalheTarefaAPIView

--- modulo_5/core/urls.py
+++ modulo_5/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ListaTarefasAPIView, EstatisticasTarefasAPIView
+from .views import ListaTarefasAPIView, EstatisticasTarefasAPIView, DetalheTarefaAPIView 
 
 app_name = 'core'
 urlpatterns = [
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
     path('tarefas/estatisticas/', EstatisticasTarefasAPIView.as_view(), name='estatisticas'),
+    path('tarefas/<int:pk>/',DetalheTarefaAPIView.as_view(),name='detalhe-tarefa'),
+
 ]
