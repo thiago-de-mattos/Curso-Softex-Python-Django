87dc6a3f205e25b38c285e739f4ab418f4760c8f
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-12-12 19:37:26 -0300

    feat: adicionado pk para logica de duplicar tarefa

--- modulo_5/core/urls.py
+++ modulo_5/core/urls.py
@@ -5,6 +5,7 @@ app_name = 'core'
 urlpatterns = [
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
     path('tarefas/estatisticas/', EstatisticasTarefasAPIView.as_view(), name='estatisticas'),
+    path('tarefas/<int:pk>/duplicar/', DetalheTarefaAPIView.as_view()),
     path('tarefas/<int:pk>/',DetalheTarefaAPIView.as_view(),name='detalhe-tarefa'),
 
 ]
