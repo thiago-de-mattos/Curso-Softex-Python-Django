448abc4cf439d78ea50704d7fd36b799f2408210
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-01-07 11:06:37 -0300

    Configuração de produção para Render

--- modulo_5/config/settings.py
+++ modulo_5/config/settings.py
@@ -176,7 +176,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Compactação e cacheamento otimizado
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
